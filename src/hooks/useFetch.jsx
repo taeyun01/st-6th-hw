01c01e91d8c7a6130206787aabb4b1fdcd33dabf
taeyun <lwe12f675@gmail.com>
2024-05-29 21:45:07 +0900

    ex4: Custom Hook 사용

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,1 +1,32 @@
+import React, { useState, useEffect } from "react";
+
 // TODO: 이곳에 커스텀훅 작성하세요.
+export const useFetch = () => {
+  const [title, setTitle] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/todos/1"
+        );
+        console.log("response:", response);
+        if (!response.ok) {
+          throw new Error("Network 오류");
+        }
+        const result = await response.json();
+        setTitle(result.title);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return { title, loading, error };
+};
